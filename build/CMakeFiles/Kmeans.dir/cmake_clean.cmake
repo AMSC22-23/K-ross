9fd6cb305807abf893816560783c76fa7b483649
ciao <leo@leo.com>
2023-11-25 11:10:57 +0000

    Divisione in cartelle e prima compilazione con cmake

---
+++ build/CMakeFiles/Kmeans.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kmeans.dir/src/kmeans.cpp.o"
+  "CMakeFiles/Kmeans.dir/src/kmeans.cpp.o.d"
+  "Kmeans"
+  "Kmeans.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kmeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
